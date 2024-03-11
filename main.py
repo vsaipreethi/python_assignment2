393f5d9e0b5e057f24237107bfa0eeccfa9d21cb
Hp <preethisaivungarala23@gmail.com>
2024-03-11 16:41:22 +0530

    python_assignment2

---
+++ main.py
@@ -0,0 +1,98 @@
+#22. Program to print the series 100, 200, 300, ..., 10000
+#i= 100
+#while (i <= 10000):
+#    print(i)
+#i += 100
+
+#23. Write programs to print the following series. 5^2, 7^2,9^2.....25^2
+#i=5
+#while(i<=25):
+ #   print(i)
+  #  i+=2
+
+#24. Write programs to print the following series. 5,10,5,10,5,10,5 for 7 times
+#a=(5,10)
+#a1=a*7
+#print(a1)
+
+#26. Write programs to print the following series.1,even,3,even,5,even,.......35,even
+#a=1;
+#b=36;
+#for i in range(a,b):
+ #   if( i % 2==0):
+  #      print("even")
+   # else:
+    #    print(i)
+
+#27. Write programs to print the following series. 1,2,factor of three,4,5,factor of three, 7,8,factor of three,..........22,23,factor of three.
+# a=1;
+# b=24
+ #for i in range(a,b):
+#   if (i % 3==0):
+ #      print("factor of three")
+  # else:
+   #    print(i)
+
+#28. Write programs to print the following series. 1,3, divisible by five, 7,9, 11,13, divisible by five,.....21,23, divisible by five
+#a=1;
+#b=24;
+#for i in range(a,b):
+ #   if (i % 5==0):
+  #      print("divisivible by five")
+   # else:
+    #    print(i)
+
+#30. Write a for loop that never ends?
+#while (True):
+ #   print("python")
+
+#LOOP ASSIGNMENT:
+
+#1)Print numbers from 1 to 5 using a "for" loop.
+#a=1;
+#b=6;
+#for i in range(a,b):
+ #   print(i)
+
+#2)Print even numbers from 2 to 10 using a "while" loop.
+#i=2;
+#while (i<=10):
+ #   print(i)
+  #  i+=2
+
+#3)Calculate the sum of all numbers from 1 to 50 using a "for" loop
+#a=1;
+#b=51;
+#sum=0;
+#for i in range(a,b):
+ # sum+=i
+ # print("the sum is", sum)
+
+#4)Print the square of numbers from 1 to 5 using a "for" loop
+#a=1;
+#b=6;
+#for i in range(a,b):
+# print(i*i)
+
+#6)Generate and print the first 5 multiples of 3 using a "for" loop.
+#a=1;
+#b=6;
+#for i in range(a,b):
+ #print(i*3)
+
+ #7)Print odd numbers from 1 to 15 using a "while" loop
+#i=1;
+#while (i<=15):
+#    print(i)
+#    i+=2
+
+#print pattern:
+#*
+#**
+#***
+#****
+
+#a=4;
+#for i in range(1,a+1):
+ #   print("*" * i)
+
